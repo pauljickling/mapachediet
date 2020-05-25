b368eabf4b1d0bd4c720f214cc5935b27630f75c
Paul Jickling <pjickling@gmail.com>
2020-05-25 15:28:26 -0700

    Testing window conditions for gatsby build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react"
 /* import { Link } from "gatsby" */
-import "../index.css";
+import "../index.css"
 
-export default () => (
-  <main>
-    <div className="container">
-      <h1 className="titleFade1">Coming soon...</h1>
-      <h2 className="titleFade2">The Mapache Diet</h2>
-    </div>
-  </main>
-)
+export default () => {
+  const width = typeof window !== `undefined` ? window.innerWidth : 0;
+
+  return (
+    <main>
+      <div className="container">
+        <h1 className="titleFade1">Coming soon...</h1>
+        <h2 className="titleFade2">The Mapache Diet</h2>
+        {console.log(width)}
+      </div>
+    </main>
+  )
+}
